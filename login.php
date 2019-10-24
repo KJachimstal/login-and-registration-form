5b32889477b49227f28aca0a51c4fdd7a945edbe
Konrad Jachimstal <KJachimstal@gmail.com>
2019-10-24 21:11:48 +0200

    Added checking user in database

--- login.php
+++ login.php
@@ -6,4 +6,25 @@
     {
         echo "Error number: ".$connection->connect_errno;
     }
+    else
+    {
+        $login = $_POST['login'];
+        $password = $_POST['password'];
+
+        $query = "SELECT * FROM users WHERE login='$login' and password='$password'";
+        if($result = $connection->query($query))
+        {
+            $user_count = $result->fetch_assoc();
+            if($user_count > 0)
+            {
+                
+            }
+            else
+            {
+                
+            }
+        }
+
+        $connection->close();
+    }
 ?>
