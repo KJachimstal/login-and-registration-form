b9da5b9a51e2d6bfa01e531e2ba6855f272ef443
Konrad Jachimstal <KJachimstal@gmail.com>
2019-10-24 20:28:54 +0200

    Completed connection to database

--- login.php
+++ login.php
@@ -1,4 +1,9 @@
 <?php
     require_once "connection.php";
-    
+    $connection = @new mysqli($host, $db_user, $db_password, $db_name);
+
+    if($connection->connect_errno!=0)
+    {
+        echo "Error number: ".$connection->connect_errno;
+    }
 ?>
